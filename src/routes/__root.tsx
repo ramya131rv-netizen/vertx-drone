05ad3a0bcf06037dfdcf36e7462ce70d5576aec4
ramya131rv-netizen <ramya131rv@gmail.com>
2026-05-06 10:00:43 +0530

    new vercel fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 
-import appCss from "../styles.css?url";
+import appCss from "../../styles.css?url";
 
 function NotFoundComponent() {
   return (
